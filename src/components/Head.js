91f60c2feeca2b9b6195dd7c912bc93d8e13ddec
Joel Thoms <jthoms@gmail.com>
2020-06-29 09:38:39 -0700

    refactor: 💡 break out styles to styles.css

--- src/components/Head.js
+++ src/components/Head.js
@@ -9,30 +9,8 @@ export const Head = ({ title, children }) => (
       href="https://unpkg.com/tailwindcss@^1.0/dist/tailwind.min.css"
       rel="stylesheet"
     />
-    <style type="text/css">{`
-    .toggle:checked + .popup {
-      display: flex;
-    }
-    .gallery {
-      display: grid;
-      grid-template-columns: repeat(auto-fit, minmax(150px, 1fr));
-      grid-gap: 0.5rem;
-    }
-    @media only screen and (max-width: 768px) {
-      .gallery {
-        grid-template-columns: repeat(auto-fit, minmax(75px, 1fr));
-      }
-    }
-    .gallery .thumb source, .gallery .thumb img {
-      height: 100%;
-      width: 100%;
-      object-fit: cover; 
-    }
-    .gallery .thumb source[data-srcset], .gallery .thumb img[data-src] {
-      height: 150px
-    }
-  `}</style>
+    <link href="styles.css" rel="stylesheet"></link>
     {children}
-    <script src="/main.js"></script>
+    <script src="main.js"></script>
   </head>
 )
